bf956259784362779a1c84215b44826bcbc77441
Akaike <akaike@student.uiwtx.edu>
2025-10-28 20:53:02 -0500

    Color pick system for vehicle customize added

---
+++ Assets/Scripts/ColorPickSVImage.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ColorPickSVImage : MonoBehaviour, IDragHandler, IPointerClickHandler
+{
+    [SerializeField] private Image pickerImage;
+    [SerializeField] private RawImage SVImage;
+    private ColorPicker mColorPicker;
+    private RectTransform mRectTransform;
+    private RectTransform mPickerTransform;
+
+
+    private void Awake()
+    {
+        SVImage = GetComponent<RawImage>();
+        mColorPicker = FindAnyObjectByType<ColorPicker>();
+        mRectTransform = GetComponent<RectTransform>();
+
+        mPickerTransform = pickerImage.GetComponent<RectTransform>();
+        mPickerTransform.position = new Vector2(-(mRectTransform.sizeDelta.x * 0.5f), -(mRectTransform.sizeDelta.y) * 0.5f);
+
+    }
+
+    private void UpdateColor(PointerEventData eventData) 
+    {
+        Vector2 localPoint;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(mRectTransform, eventData.position, eventData.pressEventCamera, out localPoint);
+
+        float width = mRectTransform.rect.width;
+        float height = mRectTransform.rect.height;
+
+        localPoint.x = Mathf.Clamp(localPoint.x, -width / 2f, width / 2f);
+        localPoint.y = Mathf.Clamp(localPoint.y, -height / 2f, height / 2f);
+
+        float xNormal = (localPoint.x + width / 2f) / width;
+        float yNormal = (localPoint.y + height / 2f) / height;
+        
+        mPickerTransform.localPosition = localPoint;
+
+        Color currentColor = Color.HSVToRGB(mColorPicker.currentHue, xNormal, yNormal);
+        pickerImage.color = currentColor;
+
+        mColorPicker.SetSV(xNormal, yNormal);
+
+        Debug.Log($"HEX {pickerImage.color}");
+
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        UpdateColor(eventData);
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        UpdateColor(eventData);
+    }
+}
---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class ColorPicker : MonoBehaviour
+{
+    public float currentHue;
+    public float currentSat;
+    public float currentVal;
+
+    [SerializeField] private RawImage mHueImage;
+    [SerializeField] private RawImage mSatValImage;
+    [SerializeField] private RawImage mOutputImage;
+
+    [SerializeField] private Slider mHueSlider;
+
+    [SerializeField] private TMP_InputField mHexInput;
+
+    [SerializeField] private Texture2D mHueTexture;
+    [SerializeField] private Texture2D mSatValTexture;
+    [SerializeField] private Texture2D mOutputTexture;
+
+    [SerializeField] Color changethiscolor;
+
+    private void Start()
+    {
+        CreateHueImage();
+        CreateSVImage();
+        CreateOutputImage();
+        UpdateOutputImage();
+    }
+    private void CreateHueImage() 
+    {
+        mHueTexture = new Texture2D(1,128);
+        mHueTexture.wrapMode = TextureWrapMode.Clamp;
+        mHueTexture.name = "HueTexture";
+
+        for (int i = 0; i < mHueTexture.height; i++)
+        {
+            mHueTexture.SetPixel(0, i, Color.HSVToRGB((float)i / mHueTexture.height, 1, 0.05f));
+        }
+
+        mHueTexture.Apply();
+        currentHue = 0;
+
+        mHueImage.texture = mHueTexture; 
+    }
+
+    private void CreateSVImage()
+    {
+        mSatValTexture = new Texture2D(128, 128);
+        mSatValTexture.wrapMode = TextureWrapMode.Clamp;
+        mSatValTexture.name = "SatValTexture";
+
+        for (int j = 0; j < mSatValTexture.height; j++) 
+        {
+            for (int k = 0; k < mSatValTexture.width; k++) 
+            {
+                mSatValTexture.SetPixel(k, j, Color.HSVToRGB(currentHue,(float) k / mSatValTexture.width, (float)j / mSatValTexture.height));
+            }
+        }
+
+        mSatValTexture.Apply();
+        currentSat = 0;
+        currentVal = 0;
+
+        mSatValImage.texture = mSatValTexture;
+    }
+
+    private void CreateOutputImage() 
+    {
+        mOutputTexture = new Texture2D(1, 16);
+        mOutputTexture.wrapMode = TextureWrapMode.Clamp;
+        mOutputTexture.name = "OutputTexture";
+
+        Color currentColor = Color.HSVToRGB(currentHue, currentSat, currentVal);
+
+        for (int i = 0; i < mOutputTexture.height; i++) 
+        {
+            mOutputTexture.SetPixel(0, i, currentColor);
+        }
+
+        mOutputTexture.Apply();
+
+        mOutputImage.texture = mOutputTexture;
+    }
+
+    private void UpdateOutputImage() 
+    {
+        Color currentColor = Color.HSVToRGB(currentHue, currentSat, currentVal);
+
+        for (int i = 0; i < mOutputTexture.height; i++) 
+        {
+            mOutputTexture.SetPixel(0, i, currentColor);
+        }
+
+        mOutputTexture.Apply();
+        mHexInput.text = ColorUtility.ToHtmlStringRGB(currentColor);
+        changethiscolor = currentColor;
+    }
+
+    public void SetSV(float S, float V) 
+    {
+        currentSat = S;
+        currentVal = V;
+        UpdateOutputImage();
+        UpdateSVImage();
+    }
+
+    public void UpdateSVImage() 
+    {
+        currentHue = mHueSlider.value;
+
+        for (int j = 0; j < mSatValTexture.height; j++)
+        {
+            for (int k = 0; k < mSatValTexture.width; k++)
+            {
+                mSatValTexture.SetPixel(k, j, Color.HSVToRGB(currentHue, (float)k / mSatValTexture.width, (float)j / mSatValTexture.height));
+            }
+        }
+
+        mSatValTexture.Apply();
+        UpdateOutputImage();
+    }
+
+    public void OnTextInput() 
+    {
+        if (mHexInput.text.Length < 6) { return; }
+
+        Color newCol;
+        if (ColorUtility.TryParseHtmlString("#" + mHexInput.text, out newCol))
+            Color.RGBToHSV(newCol, out currentHue, out currentSat, out currentVal);
+
+        mHueSlider.value = currentHue;
+        mHexInput.text = "";
+        UpdateOutputImage();
+    }
+}
